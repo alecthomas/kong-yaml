0f7533854706f14963f5fe21a1d2018f101933ca
Leo Antunes <leo@costela.net>
2021-01-25 15:01:28 +0100

    fix Loader type to match kong

--- yaml.go
+++ yaml.go
@@ -9,14 +9,14 @@ import (
 )
 
 // Loader is a Kong configuration loader for YAML.
-func Loader(r io.Reader) (kong.ResolverFunc, error) {
+func Loader(r io.Reader) (kong.Resolver, error) {
 	decoder := yaml.NewDecoder(r)
 	config := map[interface{}]interface{}{}
 	err := decoder.Decode(config)
 	if err != nil {
 		return nil, err
 	}
-	return func(context *kong.Context, parent *kong.Path, flag *kong.Flag) (interface{}, error) {
+	return kong.ResolverFunc(func(context *kong.Context, parent *kong.Path, flag *kong.Flag) (interface{}, error) {
 		// Build a string path up to this flag.
 		path := []string{}
 		for n := parent.Node(); n != nil && n.Type != kong.ApplicationNode; n = n.Parent {
@@ -25,7 +25,7 @@ func Loader(r io.Reader) (kong.ResolverFunc, error) {
 		path = append(path, flag.Name)
 		path = strings.Split(strings.Join(path, "-"), "-")
 		return find(config, path), nil
-	}, nil
+	}), nil
 }
 
 func find(config map[interface{}]interface{}, path []string) interface{} {
