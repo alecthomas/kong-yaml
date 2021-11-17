0961dc6a27a106d7af0392f0a5e8930f0acccb30
Julia Ogris <juliao@squareup.com>
2021-11-17 18:34:05 +1100

    Add YAMLFileMapper
    YAMLFileMapper implements kong.MapperValue to decode a YAML file into
    a struct field. Sample usage:
    
    	var cli struct {
    	  Profile Profile `type:"yamlfile"`
    	}
    
    	func main() {
    	  kong.Parse(&cli, kong.NamedMapper("yamlfile", kongyaml.YAMLFileMapper))
    	}
    
    Signed-off-by: Julia Ogris <juliao@squareup.com>

---
+++ mapper.go
@@ -0,0 +1,35 @@
+package kongyaml
+
+import (
+	"os"
+	"reflect"
+
+	"github.com/alecthomas/kong"
+	"gopkg.in/yaml.v2"
+)
+
+// YAMLFileMapper implements kong.MapperValue to decode a YAML file into
+// a struct field.
+//
+//    var cli struct {
+//      Profile Profile `type:"yamlfile"`
+//    }
+//
+//    func main() {
+//      kong.Parse(&cli, kong.NamedMapper("yamlfile", YAMLFileMapper))
+//    }
+var YAMLFileMapper = kong.MapperFunc(decodeYAMLFile) //nolint: gochecknoglobals
+
+func decodeYAMLFile(ctx *kong.DecodeContext, target reflect.Value) error {
+	var fname string
+	if err := ctx.Scan.PopValueInto("filename", &fname); err != nil {
+		return err
+	}
+	f, err := os.Open(fname) //nolint:gosec
+	if err != nil {
+		return err
+	}
+	defer f.Close() //nolint
+
+	return yaml.NewDecoder(f).Decode(target.Addr().Interface())
+}
